b1beaca5cdf6cd5c04281a430d6fcda4ef5dccd7
ldex <laurent.duveau+git@gmail.com>
2019-11-20 18:57:52 -0500

    First commit

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Product } from '../product.interface';
+import { ProductService } from '../product.service';
+import { Observable, EMPTY } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+
+  title = 'Products';
+//  products: Product[];
+  products$: Observable<Product[]>;
+  selectedProduct: Product;
+  errorMsg: string;
+  productsNb = 0;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
+  }
+
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
+
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
+  }
+
+  constructor(private productService: ProductService) {
+   
+  }
+
+  ngOnInit() {
+    this.products$ = this
+                      .productService
+                      .products$
+                      .pipe(
+                        tap(
+                          products => this.productsNb = products.length
+                        ),
+                        catchError(
+                          error => {
+                            this.errorMsg = error;
+                            return EMPTY;
+                          }
+                        )
+                      );
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     results => this.products = results
+    //   );
+  }
+
+}
